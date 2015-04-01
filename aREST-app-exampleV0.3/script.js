c95eb698e28c259529963871ae642b8df7848b1b
Jordan Wilson <JordanWilson@fullsail.edu>
2015-03-31 22:15:06 -0400

    Fixed the slide bar to arduino communication. Added buttons to prime new pins to receive power. Also changed the versioning structure.

---
+++ aREST-app-exampleV0.3/script.js
@@ -0,0 +1,45 @@
+// Function to control LEDs
+function buttonClick(clicked_id){
+
+    if (clicked_id == "1"){
+        $.get( "digicurl.php", {
+        pin: "7", state: "1"} );  
+    } 
+
+    if (clicked_id == "2"){
+        $.get( "digicurl.php", {
+        pin: "7", state: "0"} );  
+    } 
+
+    if (clicked_id == "3"){
+        $.get( "digicurl.php", {
+        pin: "8", state: "1"} );  
+    } 
+
+    if (clicked_id == "4"){
+        $.get( "digicurl.php", {
+        pin: "8", state: "0"} );  
+    } 
+
+}
+
+function Slider(val){
+	if (val == val){
+		$.get("anacurl.php",{
+		pin: "9", state: val} );
+	}
+}
+
+function pinPrime(pin){
+
+	if (pin == "7"){
+        $.get( "pinPrime.php", {
+        pin: "7", state: "o"} );  
+    } 
+    
+    if (pin == "8"){
+        $.get( "pinPrime.php", {
+        pin: "8", state: "o"} );  
+    } 
+
+}
