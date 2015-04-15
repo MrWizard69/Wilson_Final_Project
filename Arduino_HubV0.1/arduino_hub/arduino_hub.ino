4c42cc06a6a2be5e173638bb1fade1418dd84f9e
Jordan Wilson <JordanWilson@fullsail.edu>
2015-04-14 20:25:17 -0400

    Combined two scripts together to add motion detection to my arduino server
    I want to see what I need and don't need

---
+++ Arduino_HubV0.1/arduino_hub/arduino_hub.ino
@@ -0,0 +1,163 @@
+/* 
+  This a simple example of the aREST Library for Arduino (Uno/Mega/Due/Teensy)
+  using the Ethernet library (for example to be used with the Ethernet shield). 
+  See the README file for more details.
+ 
+  Written in 2014 by Marco Schwartz under a GPL license. 
+*/
+
+// Libraries
+#include <SPI.h>
+#include <Ethernet.h>
+#include <aREST.h>
+#include <avr/wdt.h>
+
+// Enter a MAC address for your controller below.
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0F, 0x59, 0x6B };
+
+// IP address in case DHCP fails
+IPAddress ip(192,168,2,2);
+
+// Ethernet server
+EthernetServer server(80);
+
+// Create aREST instance
+aREST rest = aREST();
+
+// Variables to be exposed to the API
+int temperature;
+int humidity;
+
+/////////////////////////////
+//MOTION SENSING VARS
+//the time we give the sensor to calibrate (10-60 secs according to the datasheet)
+int calibrationTime = 30;        
+
+//the time when the sensor outputs a low impulse
+long unsigned int lowIn;         
+
+//the amount of milliseconds the sensor has to be low 
+//before we assume all motion has stopped
+long unsigned int pause = 5000;  
+
+boolean lockLow = true;
+boolean takeLowTime;  
+
+int pirPin = 5;    //the digital pin connected to the PIR sensor's output
+int ledPin = 13;
+
+
+
+void setup(void)
+{  
+  // Start Serial
+  Serial.begin(9600);
+  
+  
+  /////////////////////////////
+//MONTION SENSING SETUP
+
+pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(pirPin, LOW);
+
+  //give the sensor some time to calibrate
+  Serial.print("calibrating sensor ");
+    for(int i = 0; i < calibrationTime; i++){
+      Serial.print(".");
+      delay(1000);
+      }
+    Serial.println(" done");
+    Serial.println("SENSOR ACTIVE");
+    delay(50);
+  
+    ///////////////////////////
+  //END MOTION SENSING SETUP
+  
+  // Init variables and expose them to REST API
+  temperature = 24;
+  humidity = 40;
+  rest.variable("temperature",&temperature);
+  rest.variable("humidity",&humidity);
+
+  // Function to be exposed
+  rest.function("led",ledControl);
+  
+  // Give name and ID to device
+  rest.set_id("007");
+  rest.set_name("MrWizard69");
+
+  // Start the Ethernet connection and the server
+  if (Ethernet.begin(mac) == 0) {
+    Serial.println("Failed to configure Ethernet using DHCP");
+    // no point in carrying on, so do nothing forevermore:
+    // try to congifure using IP address instead of DHCP:
+    Ethernet.begin(mac, ip);
+  }
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  // Start watchdog
+  wdt_enable(WDTO_4S);
+}
+
+void loop() {  
+  
+  // listen for incoming clients
+  EthernetClient client = server.available();
+  rest.handle(client);
+  wdt_reset();
+  
+}
+
+// Custom function accessible by the API
+int ledControl(String command) {
+  
+  // Get state from command
+  int state = command.toInt();
+  
+  digitalWrite(6,state);
+  return 1;
+  
+  
+  
+  ////////////////////////////
+//MOTION SENSING LOOP
+
+if(digitalRead(pirPin) == HIGH){
+       digitalWrite(ledPin, HIGH);   //the led visualizes the sensors output pin state
+       if(lockLow){  
+         //makes sure we wait for a transition to LOW before any further output is made:
+         lockLow = false;            
+         Serial.println("---");
+         Serial.print("motion detected at ");
+         Serial.print(millis()/1000);
+         Serial.println(" sec"); 
+         delay(50);
+         }         
+         takeLowTime = true;
+       }
+
+     if(digitalRead(pirPin) == LOW){       
+       digitalWrite(ledPin, LOW);  //the led visualizes the sensors output pin state
+
+       if(takeLowTime){
+        lowIn = millis();          //save the time of the transition from high to LOW
+        takeLowTime = false;       //make sure this is only done at the start of a LOW phase
+        }
+       //if the sensor is low for more than the given pause, 
+       //we assume that no more motion is going to happen
+       if(!lockLow && millis() - lowIn > pause){  
+           //makes sure this block of code is only executed again after 
+           //a new motion sequence has been detected
+           lockLow = true;                        
+           Serial.print("motion ended at ");      //output
+           Serial.print((millis() - pause)/1000);
+           Serial.println(" sec");
+           delay(50);
+           }
+       }
+  
+  
+}
