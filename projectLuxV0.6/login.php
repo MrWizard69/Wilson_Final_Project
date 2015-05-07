ce9056fffdf4a6a59248599646ef391f2dfeefbe
Jordan Wilson <JordanWilson@fullsail.edu>
2015-05-06 20:23:21 -0400

    fixed my light dimming slider, no more light flickering between high and low values. Fixed a glitch where the light switch wouldn't say on after the light dimming slider had been moved and also added a specific number of characters each input box can handle as to not overload the server

---
+++ projectLuxV0.6/login.php
@@ -0,0 +1,23 @@
+<?php
+
+  //$pin = $_GET['user'];
+  //$state = $_GET['pass'];
+
+	$url = "http://192.168.2.10/";
+	//  Initiate curl
+$ch = curl_init();
+// Disable SSL verification
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+// Execute
+$result=curl_exec($ch);
+// Closing
+curl_close($ch);
+
+// Will dump a beauty json :3
+var_dump(json_decode($result, true));
+
+?>
